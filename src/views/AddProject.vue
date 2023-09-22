a468d838e3f383cb972d83dba42e9cd42d974623
mehmet <tas349873@gmail.com>
2023-09-22 23:42:26 +0300

    css

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,5 +1,69 @@
 <template>
-  <div class="addProject">
-    <h1>Add project</h1>
-  </div>
+  <form @submit.prevent="handleSubmit">
+    <label for="">Title</label>
+    <input type="text" v-model="title" required />
+
+    <label for="">Details</label>
+    <textarea v-model="details" required> </textarea>
+    <button>Add Project</button>
+  </form>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      details: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.title, this.details;
+    },
+  },
+};
+</script>
+<style>
+form {
+  background: #fff;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 2px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+textarea {
+  padding: 10px;
+  border: 0;
+  border: 2px solid #ddd;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  border-radius: 7px;
+}
+button {
+  display: block;
+  margin: 20px auto 0;
+  background: #76dd78;
+  color: #fff;
+  padding: 10px;
+  border: 0;
+  border-radius: 7px;
+  font-size: 16px;
+}
+</style>
