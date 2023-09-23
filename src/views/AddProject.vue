e75c7db8b6fbcb288e7ccc66917168fa881e24da
mehmet <tas349873@gmail.com>
2023-09-23 20:28:14 +0300

    update

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label for="">Title</label>
     <input type="text" v-model="title" required />
 
@@ -15,6 +15,22 @@ export default {
       title: "",
       details: "",
     };
+  },
+  methods: {
+    handleSubmit() {
+      let project = {
+        title: this.title,
+        details: this.details,
+        complete: false,
+      };
+      fetch("http://localhost:3000/projects", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(project),
+      })
+        .then(() => this.$router.push("/"))
+        .catch((err) => console.log(err));
+    },
   },
 };
 </script>
